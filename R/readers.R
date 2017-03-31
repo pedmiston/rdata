98a32728a1c8059c37ff0f8a9aae82935d46a27c
Pierce Edmiston <pierce.edmiston@gmail.com>
2017-03-31 17:19:19 -0500

    Forgot to add required file_stem func

--- R/readers.R
+++ R/readers.R
@@ -2,10 +2,19 @@
 #' and assigning the resulting data frames with
 #' prefixed names.
 #'
+#' @import magrittr
 #' @export
 read_csvs <- function(directory, prefix = "", stem_func = function(x) x) {
   for (csv in list.files(directory, pattern = "*.csv", full.name = TRUE)) {
     stem <- file_stem(csv) %>% stem_func()
     assign(paste0(prefix, stem), readr::read_csv(csv), envir = globalenv())
   }
+}
+
+#' Strip the stem name off a file path.
+#'
+#' @import magrittr
+#' @export
+file_stem <- function(x) {
+  basename(x) %>% tools::file_path_sans_ext()
 }
